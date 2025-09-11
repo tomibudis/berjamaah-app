783f1f55e61870d12ee3f825481dc5b9e9fa0926
tomibudis <tomibudis@gmail.com>
2025-09-11 11:49:32 +0700

    fix: prisma monororepo plugin nextjs

--- apps/server/next.config.ts
+++ apps/server/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from 'next';
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin';
 
 const nextConfig: NextConfig = {
   typedRoutes: true,
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins.push(new PrismaPlugin());
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
